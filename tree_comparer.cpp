7a7855b2b402d6d86a4f37aac463674b485ff528
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-10-11 18:14:40 +0200

    Implement printDifferences function (first try)

--- tree_comparer.cpp
+++ tree_comparer.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 #include "tree.h"
 
+void printDifferenecs(Node* firstAST, Node* secondAST, const std::string& prefix = "") {
+    if (firstAST == nullptr || secondAST == nullptr) {
+        return;
+    }
+    if (firstAST == nullptr) {
+        std::cout << prefix << "Node added in second AST: " << secondAST->name << " " << secondAST->value << '\n';
+        for (Node* child : secondAST->children) {
+            printDifferenecs(nullptr, child, prefix + " ");
+        }
+        return;
+    }
+    if (secondAST == nullptr) {
+        std::cout << prefix << "Node removed from first AST " << firstAST->name << " " << firstAST->value << '\n';
+        for (Node* child : firstAST->children) {
+            printDifferenecs(child, nullptr, prefix + " ");
+        } 
+        return;
+    }
+    if (firstAST->name != secondAST->name || firstAST->value != secondAST->value) {
+        std::cout << prefix << "Node changed from: " << firstAST->name << " " << firstAST->value << " to: "
+                  << secondAST->name << " " << secondAST->value << '\n';
+    }
+    for (size_t i = 0; i < std::max(firstAST->children.size(), secondAST->children.size()); ++i) {
+        Node* firstChild = i < firstAST->children.size() ? firstAST->children[i] : nullptr;
+        Node* secondChild = i < secondAST->children.size() ? secondAST->children[i] : nullptr;
+
+        printDifferenecs(firstChild, secondChild, prefix + " ");
+    }
+}
+
 int main() {
     Tree firstStandardAST("../asts/first_standard_ast.txt");
     Tree secondStandardAST("../asts/second_standard_ast.txt");
