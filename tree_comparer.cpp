483056a96459058d06381a3477861db4e04e92f5
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-10-11 17:57:12 +0200

    Modify tree creation

--- tree_comparer.cpp
+++ tree_comparer.cpp
@@ -2,12 +2,9 @@
 #include "tree.h"
 
 int main() {
-    Tree* firstStandardAST = new Tree("../asts/first_standard_ast.txt");
-    Tree* secondStandardAST = new Tree("../asts/second_standard_ast.txt");
+    Tree firstStandardAST("../asts/first_standard_ast.txt");
+    Tree secondStandardAST("../asts/second_standard_ast.txt");
 
-    std::cout << firstStandardAST->getRoot()->name << " " << firstStandardAST->getRoot()->value << '\n';
-    std::cout << secondStandardAST->getRoot()->name << " " << secondStandardAST->getRoot()->value << '\n';
-
-    delete firstStandardAST;
-    delete secondStandardAST;
+    std::cout << firstStandardAST.getRoot()->name << " " << firstStandardAST.getRoot()->value << '\n';
+    std::cout << secondStandardAST.getRoot()->name << " " << secondStandardAST.getRoot()->value << '\n';
 }   
