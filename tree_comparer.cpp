6a96fe08d3d403a66d9d6dc59d5f81898c4e5ba4
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-10-11 17:27:54 +0200

    Add logs

--- tree_comparer.cpp
+++ tree_comparer.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "node_reader.h"
 
 int main() {
@@ -5,4 +6,7 @@ int main() {
 
     std::unique_ptr<Node> firstStandardAST = reader.readASTDump("../asts/first_standard_ast.txt");
     std::unique_ptr<Node> secondStandardAST = reader.readASTDump("../asts/second_standard_ast.txt");
+
+    std::cout << firstStandardAST->name << " " << firstStandardAST->value <<'\n';
+    std::cout << secondStandardAST->name << " " << secondStandardAST->value << '\n';
 }
