256f5cd9be454a1b456688e7040c77a58f20295e
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-10-11 18:24:22 +0200

    Remove testing logs

--- tree_comparer.cpp
+++ tree_comparer.cpp
@@ -35,8 +35,5 @@ int main() {
     Tree firstStandardAST("../asts/first_standard_ast.txt");
     Tree secondStandardAST("../asts/second_standard_ast.txt");
 
-    std::cout << firstStandardAST.getRoot()->name << " " << firstStandardAST.getRoot()->value << '\n';
-    std::cout << secondStandardAST.getRoot()->name << " " << secondStandardAST.getRoot()->value << '\n';
-
     printDifferences(firstStandardAST.getRoot(), secondStandardAST.getRoot());
 }   
