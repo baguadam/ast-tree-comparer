9d71b971e8814596241cd4612bf4ad7372043e9f
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-10-24 20:09:44 +0200

    improve getKey method to handle some unique situations, too

--- comparer/tree_comparer.cpp
+++ comparer/tree_comparer.cpp
@@ -102,12 +102,13 @@ Description:
     Generates a unique key for a node based on its type and information
 */
 std::string TreeComparer::getKey(Node* node, bool isDeclaration) const {
-    if (isDeclaration) {
-        // unique identifier for declarations, here we can use the usr
-        return node->kind + "_"  + node->usr; 
+    std::string nodeKey = node->kind + "|" + node->usr;
+    // for function it's important to make the key unique by adding the source location in the code
+    if (node->kind == "Function") {
+        nodeKey += "|" + node->path + "|" + std::to_string(node->lineNumber) + ":" + std::to_string(node->columnNumber);
     } else {
         // for statements we use the kind, path, line and column
-        return node->kind + "_" + node->path + "_" + std::to_string(node->lineNumber) + ":" + std::to_string(node->columnNumber); 
+        return node->kind + "|" + node->path + "|" + std::to_string(node->lineNumber) + ":" + std::to_string(node->columnNumber) + "|" + node->parent->usr; 
     }
 }
 
