c2ca3c248eb3eaf1c7d47d275f3d84fe0b055fbd
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-10-26 19:06:49 +0200

    Modify project structure -> libs in the comparer folder, initialize db properly

--- comparer/headers/database.h
+++ comparer/headers/database.h
@@ -3,20 +3,24 @@
 #define _DATABASE_H_
 
 #include <string>
+#include <memory>
 #include <SQLiteCpp/SQLiteCpp.h>
 
 class Database {
 public:
    Database(const std::string);
-   // void insertNode(int, const std::string&, const std::string&, const std::string&, const std::string&, const std::string&);
-   // void insertEdge(int, int);
+
+   void insertNode(int, const std::string&, const std::string&, const std::string&, const std::string&, const std::string&);
+   void insertEdge(int, int);
 
 private:
    SQLite::Database db;
-   // SQLite::Statement queryInsertNode;
-   // SQLite::Statement queryInsertEdge;   
+   std::unique_ptr<SQLite::Statement> queryInsertNode;
+   std::unique_ptr<SQLite::Statement> queryInsertEdge;  
 
    void createTables();
+   void clearDatabase();
+   void initializeStatements(); 
 };
 
 #endif
--- comparer/main.cpp
+++ comparer/main.cpp
@@ -2,45 +2,11 @@
 #include "./headers/tree.h"
 #include "./headers/database.h"
 #include <iostream>
-#include <filesystem>
-
-void testDatabaseOperations() {
-    // Ensure the directory exists
-    std::string dbPath = "../db/testdb.sqlite3";
-    std::filesystem::path dbDir = std::filesystem::path(dbPath).parent_path();
-    if (!std::filesystem::exists(dbDir)) {
-        std::cout << "Creating directory: " << dbDir << std::endl;
-        std::filesystem::create_directories(dbDir);
-    }
-
-    // Convert to absolute path
-    std::filesystem::path absDbPath = std::filesystem::absolute(dbPath);
-    std::cout << "Absolute database path: " << absDbPath << std::endl;
-
-    // // Check if the database file already exists
-    // if (std::filesystem::exists(absDbPath)) {
-    //     std::cout << "Database file already exists, deleting it: " << absDbPath << std::endl;
-    //     std::filesystem::remove(absDbPath);
-    // }
-
-    // Create a database object
-    std::cout << "Creating database object..." << std::endl;
-    try {
-        Database db(absDbPath.string());
-    } catch (const std::exception& e) {
-        std::cerr << "Error creating database object: " << e.what() << std::endl;
-        return;
-    }
-
-    std::cout << "Database operations completed successfully.\n";
-}
 
 int main() {
-    testDatabaseOperations();
+    Tree firstStandardAST("../../asts/first_standard_ast.txt");
+    Tree secondStandardAST("../../asts/second_standard_ast.txt");
 
-    // Tree firstStandardAST("../../asts/first_standard_ast.txt");
-    // Tree secondStandardAST("../../asts/second_standard_ast.txt");
-
-    // TreeComparer comparer(firstStandardAST.getRoot(), secondStandardAST.getRoot());
-    // comparer.printDifferences();
+    TreeComparer comparer(firstStandardAST.getRoot(), secondStandardAST.getRoot());
+    comparer.printDifferences();
 }
