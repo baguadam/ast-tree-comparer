31ff9c27c2145b50235498ca7055d9c5be3a5cf8
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-10-28 17:25:40 +0100

    modify getStmtNodes method to return a set of nodes

--- comparer/headers/tree.h
+++ comparer/headers/tree.h
@@ -3,7 +3,7 @@
 #define _TREE_H_
 
 #include <string>
-#include <vector>
+#include <unordered_set>s
 #include <unordered_map>
 #include "node.h"
 
@@ -16,7 +16,7 @@ public:
     const std::unordered_map<std::string, std::pair<Node*, bool>>& getDeclNodeMap() const;
     const std::unordered_multimap<std::string, Node*>& getStmtNodeMultiMap() const;
     const Node* getDeclNode(const std::string& nodeKey) const;
-    const std::vector<Node*> getStmtNodes(const std::string& nodeKey) const;
+    const std::unordered_set<Node*> getStmtNodes(const std::string& nodeKey) const;
     
     void markDeclNodeAsProcessed(const std::string& nodeKey);
     bool isDeclNodeProcessed(const std::string& nodeKey) const;
--- comparer/tree.cpp
+++ comparer/tree.cpp
@@ -44,11 +44,11 @@ const Node* Tree::getDeclNode(const std::string& nodeKey) const {
 Description:
     Returns the statement nodes based on the key of the declaration.
 */
-const std::vector<Node*> Tree::getStmtNodes(const std::string& nodeKey) const {
-    std::vector<Node*> stmtNodes;
+const std::unordered_set<Node*> Tree::getStmtNodes(const std::string& nodeKey) const {
+    std::unordered_set<Node*> stmtNodes;
     auto range = stmtNodeMultiMap.equal_range(nodeKey);
     for (auto it = range.first; it != range.second; ++it) {
-        stmtNodes.push_back(it->second);
+        stmtNodes.insert(it->second);
     }
     return stmtNodes;
 }
