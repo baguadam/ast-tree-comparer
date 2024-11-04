8e6bab2e089cbb2737d183a1cf24007e191c487c
Bagu Ádám <m3wdo5@inf.elte.hu>
2024-11-04 16:27:13 +0100

    refactor console logger

--- comparer/headers/loggers/console_logger.h
+++ comparer/headers/loggers/console_logger.h
@@ -8,9 +8,6 @@ class ConsoleLogger : public TreeComparerLogger {
 public:
     void logNode(const Node* node, const std::string& differencType, std::string indent = "") override;
     void logEdge(const Node* childId, const Node* parentId, std::string indent = "") override;
-
-private:
-    void logSeparators();
 };
 
 #endif
--- comparer/loggers/console_logger.cpp
+++ comparer/loggers/console_logger.cpp
@@ -5,15 +5,11 @@
 void ConsoleLogger::logNode(const Node* node, const std::string& differenceType, std::string indent) {
     std::cout << indent << "Node with difference type: " << differenceType << "\n";
     Utils::printNodeDetails(node, indent);
-    logSeparators();
+    Utils::printSeparators();
 }
 
 void ConsoleLogger::logEdge(const Node* child, const Node* parent, std::string indent) {
     std::cout << indent << "Child Node: " << Utils::getKey(child, child->type == "Declaration") 
               << " has parent Node: " << Utils::getKey(parent, parent->type == "Declaration") << "\n";
-    logSeparators();
-}
-
-void ConsoleLogger::logSeparators() {
     Utils::printSeparators();
 }
