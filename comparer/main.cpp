33d098c98433f71f0468c8f30dcf995d37dd9072
baguadam <m3wdo5@inf.elte.hu>
2024-11-13 20:42:09 +0100

    modify main so it takes the files from command line

--- comparer/main.cpp
+++ comparer/main.cpp
@@ -5,10 +5,18 @@
 #include "./headers/factories/console_logger_creator.h"
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <first_ast_file> <second_ast_file>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string firstFilePath = argv[1];
+    std::string secondFilePath = argv[2];
+
     try {
-        Tree firstStandardAST("../../asts/vector_98_win.txt");
-        Tree secondStandardAST("../../asts/vector_11_win.txt");
+        Tree firstStandardAST(firstFilePath);
+        Tree secondStandardAST(secondFilePath);
         
         // logger
         ConsoleLoggerCreator loggerCreator;
@@ -16,6 +24,7 @@ int main() {
 
         TreeComparer comparer(firstStandardAST, secondStandardAST, std::move(logger));
         comparer.printDifferences();
+        
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
