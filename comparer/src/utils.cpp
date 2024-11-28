ad3ccf7661fd771461a9afef9a56a34ebfe59c7d
baguadam <m3wdo5@inf.elte.hu>
2024-11-29 00:45:08 +0100

    reset encoding method

--- comparer/src/utils.cpp
+++ comparer/src/utils.cpp
@@ -168,3 +168,26 @@ size_t Utils::hashString(const std::string& str) {
     std::hash<std::string> hasher;
     return hasher(str);
 }
+
+/*
+Description:
+    Base64 encodes a given string, used for encoding the credentials for the Neo4j database
+*/
+std::string Utils::base64Encode(const std::string& in) {
+    static const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                            "abcdefghijklmnopqrstuvwxyz"
+                                            "0123456789+/";
+    std::string out;
+    int val = 0, valb = -6;
+    for (unsigned char c : in) {
+        val = (val << 8) + c;
+        valb += 8;
+        while (valb >= 0) {
+            out.push_back(base64_chars[(val >> valb) & 0x3F]);
+            valb -= 6;
+        }
+    }
+    if (valb > -6) out.push_back(base64_chars[((val << 8) >> (valb + 8)) & 0x3F]);
+    while (out.size() % 4) out.push_back('=');
+    return out;
+}
